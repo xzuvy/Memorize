d4893eb4233b3ef9fa0a6b5f447f7f66b9df739a
Pete <xzuvy27@gmail.com>
2023-02-02 15:33:15 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Peter Clarke on 02.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
